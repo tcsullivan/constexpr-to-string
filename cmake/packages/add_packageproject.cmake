a6040506c68749a2c1ed7e056b76c7e18f2a7a62
Arniiiii <lg3dx6fd@gmail.com>
2024-09-02 15:34:55 +0000

    update PackageProject.cmake to newest correct version

--- cmake/packages/add_packageproject.cmake
+++ cmake/packages/add_packageproject.cmake
@@ -1,12 +1,10 @@
 include(${CMAKE_CURRENT_LIST_DIR}/../getCPM.cmake)
 
-set(packageProject_VERSION 1.11.3)
+set(packageProject_VERSION 1.12.0)
 CPMAddPackage(
   NAME PackageProject.cmake
   VERSION ${packageProject_VERSION}
   # GIT_REPOSITORY "https://github.com/TheLartians/PackageProject.cmake.git" GIT_TAG
   # "v${packageProject_VERSION}"
-  GIT_TAG master
-  GIT_REPOSITORY https://github.com/Arniiiii/PackageProject.cmake
-  # URL "https://github.com/TheLartians/PackageProject.cmake/archive/refs/tags/v${packageProject_VERSION}.zip"
+  URL "https://github.com/TheLartians/PackageProject.cmake/archive/refs/tags/v${packageProject_VERSION}.zip"
 )
